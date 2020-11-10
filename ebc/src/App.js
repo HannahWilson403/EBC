1cde2b17348040c6b969ae8727dc6555661e088a
Hannah <HannahMarie-Wilson@live.nmit.ac.nz>
2020-11-11 11:03:11 +1300

    Creating a list
    creating a list

--- ebc/src/App.js
+++ ebc/src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import Alist from './Components/aList';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+     <Header/>
+      <Alist> </Alist>
     </div>
   );
 }
--- ebc/src/Components/Header.js
+++ ebc/src/Components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css';
-import logo from './Extreme Bike Club Logo.png';
+import logo from './ebcLogo.png';
 
 function Header() {
     return (
         <div className="Header">
             <header>
+            <img scr={logo} alt="Logo"/>;
             <h1>EBC</h1>
             <h3>Extreme Bike Club</h3>
             </header>
-            <img scr={logo} alt="Logo"/>;
+           
         </div>
     )
 }
